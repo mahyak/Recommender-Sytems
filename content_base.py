ca8aba171d084974951ff82c9db0d424c95a55e6
Mahya Kardani <m.kardaani@gmail.com>
2019-11-17 13:45:34 -0500

    Add files via upload

---
+++ content_base.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 20 13:09:48 2019
+
+@author: mahya
+"""
+
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel 
+
+def item(id):
+    return dataset.loc[dataset['id'] == id]['description'].tolist()[0].split('-')[0]
+
+def recommend(item_id, num):
+    print("Recommending " + str(num) + " products similar to " + item(item_id) + "...")   
+    print("-------")    
+    recs = results[item_id][:num]   
+    for rec in recs: 
+       print("Recommended: " + item(rec[1]) + " (score:" +      str(rec[0]) + ")")
+
+dataset = pd.read_csv('sample-data.csv')
+tf = TfidfVectorizer(analyzer='word', ngram_range=(1,3), min_df=0, stop_words='english')
+tfidf_matrix = tf.fit_transform(dataset['description'])
+
+cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
+results = {}
+for idx, row in dataset.iterrows():
+    similar_indices = cosine_similarities[idx].argsort()[:-100:-1] 
+    similar_items = [(cosine_similarities[idx][i], dataset['id'][i])
+    for i in similar_indices]
+    results[row['id']] = similar_items[1:]
+    
+recommend(item_id=11, num=5)
+    
